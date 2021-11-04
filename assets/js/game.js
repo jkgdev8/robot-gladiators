1d2805f5f940f1b2e2ab9ba817ddd8ebdb5ceebb
Jkgdev8 <juliennegarcia@Juliennes-MBP.socal.rr.com>
2021-11-03 22:36:36 -0700

    fixed player name prompt

--- assets/js/game.js
+++ assets/js/game.js
@@ -78,6 +78,7 @@ var startGame = function() {
     if (playerInfo.health > 0) {
       // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
       window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
+      
 
       // pick new enemy to fight based on the index of the enemy.names array
       var pickedEnemyObj = enemyInfo[i];
@@ -170,7 +171,24 @@ var randomNumber = function(min,max) {
   return value;
 };
 
+// function to set name
+var getPlayerName = function() {
+  var name = "";
+
+  // ***************************************
+  // ADD LOOP HERE WITH PROMPT AND CONDITION
+  // ***************************************
+while (name === "" || name === null) {
+  name = prompt("What is your robot's name?");
+}
+
+  console.log("Your robot's name is " + name);
+  return name;
+  
+};
+
 var playerInfo = {
+  name: getPlayerName(),
   name: window.prompt("What is your robot's name?"),
   health: 100,
   attack: 10,
