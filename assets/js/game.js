5e5c335b7e7a2e8671f0e014b65c190b152477da
Jkgdev8 <juliennegarcia@Juliennes-MBP.socal.rr.com>
2021-11-04 15:12:03 -0700

    added random fight feature order

--- assets/js/game.js
+++ assets/js/game.js
@@ -73,7 +73,7 @@ var fight = function(enemy) {
           " health remaining."
       );
 
-      // check enemy's health
+      // check enemy's health///////
       if (enemy.health <= 0) {
         window.alert(enemy.name + " has died!");
 
